0a00a1d5860a28646f7127e625fa47dacd1fdbb2
devrajneupane <asminneupane79@gmail.com>
2021-10-21 17:26:15 +0545

    Resolved Symlinks

--- Sublime Text/open_in_terminus.py
+++ Sublime Text/open_in_terminus.py
@@ -1,1 +1,25 @@
-/mnt/c/Users/LENOVO/AppData/Roaming/Sublime Text/Packages/User/open_in_terminus.py
+import sublime
+import sublime_plugin
+
+import os
+
+
+class OpenInTerminusCommand(sublime_plugin.WindowCommand):
+    """
+    Drop in replacement for terminus_open that will open the 
+    """
+    def run(self, dirs, panel=True, **kwargs):
+        command = "terminus_open"
+        kwargs["cwd"] = dirs[0]
+
+        if panel:
+            command = "toggle_terminus_panel"
+            kwargs["panel_name"] = "Terminus: %s" % os.path.split(dirs[0])[-1]
+
+        self.window.run_command(command, kwargs)
+
+    def is_enabled(self, dirs, panel=True):
+        return len(dirs) == 1
+
+    def is_visible(self, dirs, panel=True):
+        return len(dirs) == 1
--- Windows Terminal/Powershell/Microsoft.PowerShell_profile.ps1
+++ Windows Terminal/Powershell/Microsoft.PowerShell_profile.ps1
@@ -1,1 +1,2 @@
-/mnt/c/Users/LENOVO/Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+oh-my-posh --init --shell pwsh --config C:\Users\LENOVO\AppData\Local\Programs\oh-my-posh\themes\ohmyposhv3-v2.json | Invoke-Expression
+Import-Module -Name Terminal-Icons
