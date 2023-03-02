3aeb40ad171cdd6125179b3cfd8b39d17283e34c
devrajneupane <asminneupane79@gmail.com>
2023-03-02 20:51:39 +0545

    feat: switch to lazy plugin manager

--- init.lua
+++ init.lua
@@ -1,1 +1,10 @@
-require 'dev'
+require("config.options")
+require("config.lazy")
+
+vim.api.nvim_create_autocmd("User", {
+    pattern = "VeryLazy",
+    callback = function()
+        require("config.autocmds")
+        require("config.keymaps")
+    end,
+})
