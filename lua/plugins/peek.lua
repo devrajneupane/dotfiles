25105f739447ef3bf675628f7df67c92bb797a92
devrajneupane <asminneupane79@gmail.com>
2023-04-03 20:40:08 +0545

    feat(markdown): add peek.nvim

---
+++ lua/plugins/peek.lua
@@ -0,0 +1,18 @@
+return {
+    "toppair/peek.nvim",
+    build = "deno task --quiet build:fast",
+    keys = {
+      {
+        "<leader>mp",
+        function()
+          local peek = require("peek")
+          if peek.is_open() then
+            peek.close()
+          else
+            peek.open()
+          end
+        end,
+        desc = "Peek (Markdown Preview)",
+      },
+    },
+  }
