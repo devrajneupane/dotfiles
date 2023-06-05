0efcda24d7dd74558f841feaac2faef58aae421a
devrajneupane <asminneupane79@gmail.com>
2023-06-05 23:48:58 +0545

    feat: add dail.nvim

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -16,4 +16,29 @@ return {
 			require("nvim-surround").setup()
 		end,
 	},
+    -- enhanced increment/decrement
+    {
+        "monaqa/dial.nvim",
+        config = function()
+            local augend = require("dial.augend")
+            require("dial.config").augends:register_group({
+                default = {
+                    augend.integer.alias.decimal_int,
+                    augend.integer.alias.hex,
+                    augend.integer.alias.octal,
+                    augend.integer.alias.binary,
+                    augend.date.alias["%Y/%m/%d"],
+                    augend.date.alias["%Y-%m-%d"],
+                    augend.constant.alias.bool,
+                    augend.semver.alias.semver,
+                    augend.constant.new({ elements = { "let", "const" } }),
+                },
+            })
+        end,
+        keys = {
+            { "<C-a>", function() return require("dial.map").inc_normal() end, expr = true, desc = "Increment" },
+            { "<C-x>", function() return require("dial.map").dec_normal() end, expr = true, desc = "Decrement" },
+        },
+    },
+
 }
