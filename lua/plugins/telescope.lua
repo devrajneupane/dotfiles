d1bbc385f8995c77fb676d32142bc4aed977ae6c
devrajneupane <asminneupane79@gmail.com>
2023-06-06 23:12:04 +0545

    feat: add dressing.nvim

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,22 +4,22 @@ return {
     version = false,
     dependencies = {
         "nvim-lua/plenary.nvim",
-        "nvim-telescope/telescope-ui-select.nvim",
         "nvim-telescope/telescope-live-grep-args.nvim",
         "nvim-telescope/telescope-symbols.nvim",
         { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
     },
     opts = function()
         local actions = require("telescope.actions")
-        local theme = require("telescope.themes")
         local lga_actions = require("telescope-live-grep-args.actions")
         local trouble = require("trouble.providers.telescope")
         return {
             defaults = {
-                prompt_prefix = " ",
+                prompt_prefix = "  ",
                 initial_mode = "normal",
                 layout_strategy = "horizontal",
                 sorting_strategy = "ascending",
+                selection_caret = ' ',
+                file_ignore_patterns = { "^%.git/", "^node_modules/", ".git/" },
                 layout_config = {
                     horizontal = {
                         preview_width = 0.55,
@@ -62,11 +62,6 @@ return {
                             },
                         },
                     },
-                    ["ui-select"] = {
-                        theme.get_dropdown({
-                            -- even more opts
-                        }),
-                    },
                 },
             },
         }
@@ -76,40 +71,39 @@ return {
         telescope.setup(opts)
         telescope.load_extension("fzf")
         telescope.load_extension("live_grep_args")
-        telescope.load_extension("ui-select")
         telescope.load_extension("noice")
     end,
     keys = function()
         local cmd = require("utils").cmd
         return {
-            { "<leader>ff", cmd("Telescope find_files"),                desc = "find files" },
-            { "<leader>fb", cmd("Telescope buffers"),                   desc = "find buffers" },
-            { "<leader>fr", cmd("Telescope oldfiles"),                  desc = "find recent files" },
-            { "<leader>fg", cmd("Telescope live_grep"),                 desc = "find in files (grep)" },
-            { "<leader>fs", cmd("Telescope grep_string"),               desc = "grep string in cwd" },
-            { "<leader>fl", cmd("Telescope live_grep_args"),            desc = "find in files (grep with args)" },
-            { "<leader>fh", cmd("Telescope help_tags"),                 desc = "help tags" },
+            { "<leader>ff", cmd("Telescope find_files"), desc = "find files" },
+            { "<leader>fb", cmd("Telescope buffers theme=dropdown previewer=false"), desc = "find buffers" },
+            { "<leader>fr", cmd("Telescope oldfiles"), desc = "find recent files" },
+            { "<leader>fg", cmd("Telescope live_grep"), desc = "find in files (grep)" },
+            { "<leader>fs", cmd("Telescope grep_string"), desc = "grep string in cwd" },
+            { "<leader>fl", cmd("Telescope live_grep_args"), desc = "find in files (grep with args)" },
+            { "<leader>fh", cmd("Telescope help_tags"), desc = "help tags" },
             { "<leader>fz", cmd("Telescope current_buffer_fuzzy_find"), desc = "fuzzy search (current buffer)" },
-            { "<leader>:",  cmd("Telescope commands"),                  desc = "plugin/user commands" },
-            { "<leader>f:", cmd("Telescope commands_history"),          desc = "commands history" },
-            { "<leader>fH", cmd("Telescope search_history"),            desc = "search history" },
-            { "<leader>fd", cmd("Telescope diagnostics"),               desc = "search history" },
-            { "<leader>fa", cmd("Telescope autocommands"),              desc = "Autocommands" },
-            { "<leader>fv", cmd("Telescope vim_options"),               desc = "Options" },
-            { "<leader>fm", cmd("Telescope marks"),                     desc = "marks and their value" },
-            { "<leader>fM", cmd("Telescope man_pages"),                 desc = "man pages" },
-            { "<leader>fR", cmd("Telescope registers"),                 desc = "Registers" },
-            { "<leader>fT", cmd("Telescope tags"),                      desc = "tags (cwd)" },
-            { "<leader>ft", cmd("Telescope current_buffer_tags"),       desc = "tags (current buf)" },
+            { "<leader>:", cmd("Telescope commands"), desc = "plugin/user commands" },
+            { "<leader>f:", cmd("Telescope command_history"), desc = "commands history" },
+            { "<leader>fH", cmd("Telescope search_history"), desc = "search history" },
+            { "<leader>fd", cmd("Telescope diagnostics"), desc = "search history" },
+            { "<leader>fa", cmd("Telescope autocommands"), desc = "Autocommands" },
+            { "<leader>fv", cmd("Telescope vim_options"), desc = "Options" },
+            { "<leader>fm", cmd("Telescope marks"), desc = "marks and their value" },
+            { "<leader>fM", cmd("Telescope man_pages"), desc = "man pages" },
+            { "<leader>fR", cmd("Telescope registers"), desc = "Registers" },
+            { "<leader>fT", cmd("Telescope tags"), desc = "tags (cwd)" },
+            { "<leader>ft", cmd("Telescope current_buffer_tags"), desc = "tags (current buf)" },
 
             -- git
-            { "<leader>gf", cmd("Telescope git_files"),                 desc = "git files 🔭" },
-            { "<leader>gC", cmd("Telescope git_files"),                 desc = "git commits 🔭" },
-            { "<leader>gb", cmd("Telescope git_branches"),              desc = "git branches 🔭" },
-            { "<leader>gs", cmd("Telescope git_status"),                desc = "git status 🔭" },
+            { "<leader>gf", cmd("Telescope git_files"), desc = "git files 🔭" },
+            { "<leader>gC", cmd("Telescope git_files"), desc = "git commits 🔭" },
+            { "<leader>gb", cmd("Telescope git_branches"), desc = "git branches 🔭" },
+            { "<leader>gs", cmd("Telescope git_status"), desc = "git status 🔭" },
 
             -- Misc
-            { "<leader>fB", cmd("Telescope builtin theme=dropdown"),    desc = "builtins 🔭" },
+            { "<leader>fB", cmd("Telescope builtin theme=dropdown"), desc = "builtins 🔭" },
             {
                 "<leader>f.",
                 function()
@@ -118,8 +112,8 @@ return {
                         search_dirs = { vim.fn.stdpath("config") },
                     })
                 end,
-                desc = "Find Files (Neovim Config)",
-            },
-        }
+			desc = "Find Files (Neovim Config)",
+		},
+	}
     end,
 }
--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -22,4 +22,18 @@ return {
 			{ "<leader>=", cmd("WindowsEqualize"), desc = "windows: maximize vertically" },
 		},
 	},
+    -- imporve the default vim.ui interfaces
+    {
+        "stevearc/dressing.nvim",
+        init = function()
+            vim.ui.select = function(...)
+                require("lazy").load({ plugins = { "dressing.nvim" } })
+                return vim.ui.select(...)
+            end
+            vim.ui.input = function(...)
+                require("lazy").load({ plugins = { "dressing.nvim" } })
+                return vim.ui.input(...)
+            end
+        end,
+    },
 }
