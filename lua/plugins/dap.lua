d29e1c39ccbdf86fbfbf17c5e817ad9c084b35ce
devrajneupane <asminneupane79@gmail.com>
2023-06-16 23:36:50 +0545

    feat: add nvim-dap and adpaters for lua and python

---
+++ lua/plugins/dap.lua
@@ -0,0 +1,91 @@
+-- FIX: fix bug's related to DAP widgets
+return {
+    {
+        "mfussenegger/nvim-dap",
+        keys = {
+            { "<leader>db", function() require("dap").toggle_breakpoint() end, desc = "Toggle Breakpoint" },
+            { "<leader>dB", function() require("dap").set_breakpoint(vim.fn.input('Breakpoint condition: ')) end, desc = "Breakpoint Condition" },
+            { "<leader>dc", function() require("dap").continue() end, desc = "Continue" },
+            { "<leader>dC", function() require("dap").run_to_cursor() end, desc = "Run to Cursor" },
+            { "<leader>dg", function() require("dap").goto_() end, desc = "Go to line (no execute)" },
+            { "<leader>di", function() require("dap").step_into() end, desc = "Step Into" },
+            { "<leader>dj", function() require("dap").down() end, desc = "Down" },
+            { "<leader>dk", function() require("dap").up() end, desc = "Up" },
+            { "<leader>dl", function() require("dap").run_last() end, desc = "Run Last" },
+            { "<leader>do", function() require("dap").step_out() end, desc = "Step Out" },
+            { "<leader>dO", function() require("dap").step_over() end, desc = "Step Over" },
+            { "<leader>dp", function() require("dap").pause() end, desc = "Pause" },
+            { "<leader>dr", function() require("dap").repl.toggle() end, desc = "Toggle REPL" },
+            { "<leader>ds", function() require("dap").session() end, desc = "Session" },
+            { "<leader>dt", function() require("dap").terminate() end, desc = "Terminate" },
+            { "<leader>dw", function() require("dap.ui.widgets").hover() end, desc = "Widgets" },
+        },
+    },
+    {
+        "rcarriga/nvim-dap-ui",
+        dependencies = "mfussenegger/nvim-dap",
+        config = function()
+            local dap = require("dap")
+            local dapui = require("dapui")
+            dapui.setup()
+            dap.listeners.after.event_initialized["dapui_config"] = function() dapui.open({ }) end
+            dap.listeners.before.event_terminated["dapui_config"] = function() dapui.close({ }) end
+            dap.listeners.before.event_exited["dapui_config"] = function() dapui.close({ }) end
+        end,
+        keys = {
+            { "<leader>du", function() require("dapui").toggle({ }) end, desc = "Dap UI" },
+            { "<leader>de", function() require("dapui").eval() end, desc = "Eval", mode = { "n", "v" } },
+        },
+    },
+    {
+      "theHamsta/nvim-dap-virtual-text",
+      config = true,
+    },
+    {
+        "rcarriga/cmp-dap",
+        dependencies =  "nvim-cmp" ,
+        config = function()
+            require("cmp").setup.filetype({ "dap-repl", "dapui_watches", "dapui_hover" }, {
+                sources = {
+                    { name = "dap" },
+                },
+            })
+        end
+    },
+    {
+        "mfussenegger/nvim-dap-python",
+        ft = "python",
+        dependencies = {
+            "mfussenegger/nvim-dap",
+            "rcarriga/nvim-dap-ui",
+        },
+        config = function()
+            local path = "~/.local/share/nvim/mason/packages/debugpy/venv/bin/python"
+            require("dap-python").setup(path)
+        end,
+        keys = {
+            { "<leader>dap", function() require("dap-python").test_method() end, desc = "debug python run" },
+        },
+    },
+    {
+        "jbyuki/one-small-step-for-vimkind",
+        ft = 'lua',
+        config = function()
+            local dap = require("dap")
+            dap.configurations.lua = {
+                {
+                    type = "nlua",
+                    request = "attach",
+                    name = "Attach to running Neovim instance",
+                },
+            }
+            dap.adapters.nlua = function(callback, config)
+                callback({ type = "server", host = config.host or "127.0.0.1", port = config.port or 8086 })
+            end
+        end,
+        keys = {
+            { "<leader>daL", function() require("osv").launch({ port = 8086 }) end, desc = "Adapter Lua Server" },
+            { "<leader>dal", function() require("osv").run_this() end, desc = "Adapter Lua" },
+        },
+    }
+}
