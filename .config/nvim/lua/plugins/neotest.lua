00eebc7f09c1ce24a1a33a97223544972a0db125
devrajneupane <asminneupane79@gmail.com>
2024-11-15 17:57:23 +0545

    chore(neotest): add neotest adapter provided by rustaceanvim for `rust` and other configs

--- .config/nvim/lua/plugins/neotest.lua
+++ .config/nvim/lua/plugins/neotest.lua
@@ -1,53 +1,66 @@
--- WIP
 return {
-    { "nvim-neotest/neotest-python" },
-    { "rouge8/neotest-rust" },
-    {
-        "nvim-neotest/neotest",
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-            "nvim-treesitter/nvim-treesitter",
+    -- An extensible framework for interacting with tests within NeoVim
+    "nvim-neotest/neotest",
+    dependencies = {
+        "nvim-neotest/nvim-nio",
+        "nvim-treesitter/nvim-treesitter",
+        -- Adpaters
+        "nvim-neotest/neotest-python",
+    },
+    opts = {
+        status = { signs = false, virtual_text = true },
+        output = { open_on_run = true },
+        icons = {
+            failed = "✘",
+            passed = "✔",
+            running_animated = { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" },
         },
-        opts = {
-            adapters = {
-                [ "neotest-python" ] = {
-                    dap = { justMyCode = false, console = "integratedTerminal", subProcess = false },
-                    pytest_discovery = true,
-                },
-                [ "neotest-rust" ] = {},
-            },
-            status = { virtual_text = true },
-            icons = { running_animated = { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" } },
-            strategies = { integrated = { width = 180 } },
-            quickfix = {
-                open = function()
-                    if require("lazy.core.config").plugins["trouble.nvim"] ~= nil then
-                        -- vim.cmd("Trouble quickfix")
-                        vim.cmd.Trouble("quickfix")
-                    else
-                        vim.cmd("copen")
-                    end
-                end,
-            },
+        strategies = { integrated = { width = 180 } },
+        discovery = {
+            filter_dir = function(dir) return not vim.startswith(dir, "node_modules") end,
         },
-        keys = {
-            {"[n", function() require('neotest').jump.prev({ status = "failed" }) end, desc = "previous failed test"},
-            {"]n", function() require('neotest').jump.next({ status = "failed" }) end, desc = "next failed test"},
-            { "<leader>ta", function() require("neotest").run.attach() end, desc = "Attach to nearest test" },
-            { "<leader>tr", function() require("neotest").run.run(vim.fn.expand("%")) end, desc = "Run current test file" },
-            { "<leader>tR", function() require("neotest").run.run(vim.loop.cwd()) end, desc = "Run all test files" },
-            { "<leader>tN", function() require("neotest").run.run() end, desc = "Run nearest test" },
-            { "<leader>tT", function() require("neotest").summary.toggle() end, desc = "Toggle summary" },
-            { "<leader>to", function() require("neotest").output.open({ enter = true, auto_close = true }) end, desc = "Show output" },
-            { "<leader>tO", function() require("neotest").output_panel.toggle() end, desc = "Toggle output panel" },
-            { "<leader>tP", function() require("neotest").run.stop() end, desc = "Stop nearest test" },
+        quickfix = {
+            open = function()
+                if require("lazy.core.config").plugins["trouble.nvim"] ~= nil then
+                    vim.cmd("Trouble quickfix")
+                else
+                    vim.cmd("copen")
+                end
+            end,
         },
     },
-    {
-        "mfussenegger/nvim-dap",
-        optional = true,
-        keys = {
-            { "<leader>td", function() require("neotest").run.run({strategy = "dap"}) end, desc = "Debug nearest" },
-        },
+    config = function(_, opts)
+        opts = vim.tbl_deep_extend("force", {
+            adapters = {
+                require("rustaceanvim.neotest"),
+                require("neotest-python")({
+                    dap = { justMyCode = false, console = "integratedTerminal", subProcess = false },
+                    pytest_discover_instances = true,
+                }),
+            },
+        }, opts)
+        require("neotest").setup(opts)
+    end,
+    -- stylua: ignore
+    keys = {
+        { "<leader>o", "", desc = "neotest" },
+        { "<leader>oo", "", desc = "output" },
+        { "[n", function() require("neotest").jump.prev() end, desc = "previous test" },
+        { "]n", function() require("neotest").jump.next() end, desc = "next test" },
+        { "[N", function() require("neotest").jump.prev({ status = "failed" }) end, desc = "previous failed test" },
+        { "]N", function() require("neotest").jump.next({ status = "failed" }) end, desc = "next failed test" },
+        { "<leader>oa", function() require("neotest").run.attach() end, desc = "attach to nearest test" },
+        { "<leader>on", function() require("neotest").run.run() end, desc = "run nearest test" },
+        { "<leader>ob", function() require("neotest").run.run(vim.fn.expand("%")) end, desc = "run tests in %" },
+        { "<leader>oc", function() require("neotest").run.run(vim.uv.cwd()) end, desc = "run tests in cwd" },
+        { "<leader>od", function() require("neotest").run.run({vim.fn.expand("%"), strategy = "dap"}) end, desc = "debug curren file" },
+        { "<leader>ol", function() require("neotest").run.run_last() end, desc = "run last test" },
+        { "<leader>os", function() require("neotest").summary.toggle() end, desc = "toggle summary" },
+        { "<leader>om", function() require("neotest").summary.run_marked() end, desc = "run all marked positions" },
+        { "<leader>ooo", function() require("neotest").output.open({ enter = true, auto_close = true }) end, desc = "show output" },
+        { "<leader>oop", function() require("neotest").output_panel.toggle() end, desc = "toggle output panel" },
+        { "<leader>ooc", function() require("neotest").output_panel.clear() end, desc = "clear output panel" },
+        { "<leader>ot", function() require("neotest").run.stop() end, desc = "stop nearest test" },
+        { "<leader>ow", function() require("neotest").watch.toggle(vim.fn.expand("%")) end, desc = "toggle watching in %" },
     },
 }
